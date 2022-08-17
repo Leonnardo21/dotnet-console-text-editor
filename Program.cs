8b6d4446e32ddfe589e5b1b11c09e12170242104
Leonardo de Souza Rodrigues <leosouza1392@gmail.com>
2022-08-17 00:06:27 -0300

    feat: Implemented create function

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ namespace TextEditor
             Console.WriteLine("1 - Abrir arquivo");
             Console.WriteLine("2 - Criar novo arquivo");
             Console.WriteLine("0 - Sair");
+            Console.WriteLine("--------------------------");
             short option = short.Parse(Console.ReadLine());
 
             switch (option)
@@ -29,6 +30,21 @@ namespace TextEditor
         }
 
         static void Open() { }
-        static void Edit() { }
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("------------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Write(text);
+        }
     }
 }
